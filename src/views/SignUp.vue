fde8fcda81f6976ccc629b67b1aa2e6b228652a2
Under <jorgeolmo.I@gmail.com>
2021-10-11 10:25:35 +0200

    SignUp Visual

---
+++ src/views/SignUp.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="mx-auto" style="max-width: 500px">
+      <v-overlay :value="loading">
+          <v-progress-circular color="primary" indeterminate size="64"></v-progress-circular>
+      </v-overlay>
+      <v-form ref="form" v-model="form" class="pa-4 mt-6">
+          <v-text-field v-model="name" :rules="[validation.required]" filled label="Nombre"></v-text-field>
+          <v-text-field v-model="email" :rules="[validation.email]" filled label="Correo electronico" type="email"></v-text-field>
+          <v-text-field v-model="password" :rules="[validation.password, validation.length(6)]" filled label="Contraseña" type="password" counter="6" style="min-height: 96px"></v-text-field>
+      </v-form>
+      <v-alert v-if="errorMsg" border="top" color="red" class="ml-5 mr-5">
+          {{errorMsg}}
+      </v-alert>
+      <v-divider></v-divider>
+      <v-card-actions>
+          <v-btn text @click="$refs.form.reset()">Limpiar formulario</v-btn>
+          <v-spacer/>
+          <v-btn :disabled="!form" color="primary" :loading="loading" depressed @click="signUp">Registrate</v-btn>
+      </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: '',
+        email: '',
+        password: '',
+        errorMsg: '',
+        formValidation: false,
+        loading: false,
+        validation: {
+            email: v => !!(v || '').match(/@/) || 'Por favor introduce un email valido',
+            length: len => v => (v || '').length >= len || `La contraseña es demasiado corta, necesita ${len} caracteres`,
+            password: v => !!(v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) || 'La contraseña debe contener al menos mayuscula, minuscula, numero y caracter especial',
+            required: v => !!v || 'Este campo es obligatorio'
+
+        }
+    }),
+    methods: {
+        signUp() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
